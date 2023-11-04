d9b8119523bbb2417eff0b44cfad3567af56ff4e
Jefferson Alves <jefff9910@outlook.com>
2023-11-04 12:47:32 -0300

    Header, routes e pages

---
+++ src/pages/Filme/index.jsx
@@ -0,0 +1,10 @@
+
+
+
+function Filme() {
+    return (<div>
+        <h1>FILMES</h1>
+    </div>)
+}
+
+export default Filme;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+
+
+
+function Home() {
+    return (<>
+        <h1>BEM-VINDO A HOME</h1>
+    </>)
+
+}
+
+
+export default Home;
