9c97a84411621d806dab6908d2a1522b04b794ec
Jefferson Alves <jefff9910@outlook.com>
2023-11-05 20:24:49 -0300

    Listando filmes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,45 @@
-
+import {useEffect, useState} from 'react'
+import api from '../../services/app.js'
+import { Link } from 'react-router-dom'
+import './home.css'
 
 
 function Home() {
-    return (<>
-        <h1>BEM-VINDO A HOME</h1>
-    </>)
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(()=> {
+        async function loadFilmes() {
+            const response = await api.get('/movie/now_playing', {
+                params: {
+                    api_key: '922829448eba551831579cc4c9f92cd6',
+                    language: 'pt-BR',
+                    page: 1
+                }
+            });
+          
+            setFilmes(response.data.results.slice(0, 10))
+        }
+
+        loadFilmes();
+    }, [])
+
+    return (
+        <div className='container'>
+            <div className='lista-filmes'>
+                {
+                    filmes.map((filmes) => {
+                        return (
+                            <article key={filmes.id}>
+                                <strong>{filmes.title}</strong>
+                                <img src={`https://image.tmdb.org/t/p/original/${filmes.backdrop_path}`} alt={filmes.title} />
+                                <Link to={`/filme/${filmes.id}`}>Acessar</Link>
+                            </article>
+                        )
+                    })    
+                }
+            </div>
+        </div>    
+    )
 
 }
 
---
+++ src/services/app.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+// BASE URL
+// https://api.themoviedb.org/3/
+
+
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/'
+
+})
+
+export default api;
